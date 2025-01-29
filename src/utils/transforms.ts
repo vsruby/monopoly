226799c8315836dca8492add996153bef81e78a0
Vince Ruby <vsruby91@gmail.com>
2025-01-28 21:15:25 -0500

    add transforms for players and turns

--- src/utils/transforms.ts
+++ src/utils/transforms.ts
@@ -1,14 +1,46 @@
 import { InferSelectModel } from 'drizzle-orm';
-import { games } from '../db/schemas/index.js';
+import { games, players, turns } from '../db/schemas/index.js';
 
 type Game = InferSelectModel<typeof games>;
+type Player = InferSelectModel<typeof players>;
+type Turn = InferSelectModel<typeof turns>;
+
+function transformDate(date?: Date | null) {
+  return date ? date.toISOString() : null;
+}
+
 export function transformGame(game: Game) {
   return {
     id: game.id,
     hostId: game.hostId,
     round: game.round,
     state: game.state,
-    createdAt: game.createdAt,
-    updatedAt: game.updatedAt,
+    createdAt: transformDate(game.createdAt),
+    updatedAt: transformDate(game.updatedAt),
+  };
+}
+
+export function transformPlayer(player: Player) {
+  return {
+    id: player.id,
+    avatar: player.avatar,
+    currentTileId: player.currentTileId,
+    money: player.money,
+    order: player.order,
+    createdAt: transformDate(player.createdAt),
+    updatedAt: transformDate(player.updatedAt),
+  };
+}
+
+export function transformTurn(turn: Turn) {
+  return {
+    id: turn.id,
+    endTileId: turn.endTileId,
+    isFinished: turn.isFinished,
+    phase: turn.phase,
+    round: turn.round,
+    startTileId: turn.startTileId,
+    createdAt: transformDate(turn.createdAt),
+    updatedAt: transformDate(turn.updatedAt),
   };
 }
